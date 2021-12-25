4872d828a219f69faa1dd7fa89bbacdaf2e09c14
Abdelwahab Hussein <abdelwahab.huss07@gmail.com>
2021-12-25 22:33:46 +0200

    edit middleware

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -6,23 +6,16 @@ export async function middleware(req) {
   const token = await getToken({ req, secret: process.env.JWT_SECRET })
   const { pathname } = req.nextUrl
 
-  console.log('PATHNAME IS', pathname)
-  console.log('TOKEN IS', token)
+  //   Allow requests if the following is true...
 
-  // If user is logged in, redirect to home
-  if (token && pathname.includes('/login')) {
-    return NextResponse.redirect('/')
-  }
-
-  // Allow requests if the following is true...
   // 1) A token exists
-  // 2) Its a request for next-auth session & provider fetching
   if (pathname.includes('/api/auth') || token) {
     return NextResponse.next()
   }
 
-  // Redirect them to login if they dont have token AND are requesting a protected route
-  if (!token && !pathname.includes('/login')) {
+  //   2) Its a request for next-auth session & provider fetching
+  //   Redirect them to login if they dont have token AND are requesting a protected route
+  if (!token && pathname !== '/login') {
     return NextResponse.redirect('/login')
   }
 }
